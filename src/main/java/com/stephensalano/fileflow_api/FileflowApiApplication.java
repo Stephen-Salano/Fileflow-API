4c622f6ae78fe345091bea055d63f569c9a1a957
Mo <mollykemto@gmail.com>
2025-05-08 21:17:31 +0300

    initial commit: Spring Boot project setup

---
+++ src/main/java/com/stephensalano/fileflow_api/FileflowApiApplication.java
@@ -0,0 +1,13 @@
+package com.stephensalano.fileflow_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileflowApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileflowApiApplication.class, args);
+	}
+
+}
