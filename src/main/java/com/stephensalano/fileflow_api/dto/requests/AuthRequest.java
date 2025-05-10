1472d9d04424522043ad14419beffe3fad2af37f
Mo <mollykemto@gmail.com>
2025-05-10 16:51:13 +0300

    Completed Initial File and folder structure

---
+++ src/main/java/com/stephensalano/fileflow_api/dto/requests/AuthRequest.java
@@ -0,0 +1,4 @@
+package com.stephensalano.fileflow_api.dto.requests;
+
+public class AuthRequest {
+}
---
+++ src/main/java/com/stephensalano/fileflow_api/services/auth/AuthService.java
@@ -0,0 +1,4 @@
+package com.stephensalano.fileflow_api.services.auth;
+
+public interface AuthService {
+}
