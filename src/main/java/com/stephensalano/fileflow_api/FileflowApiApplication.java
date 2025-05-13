4c9e2877cfb482a81baf8d599183ea722d4efa6e
Salano <xirlanostephen@gmail.com>
2025-05-13 14:23:01 +0300

    Added Soring Profiles for different environments

--- src/main/java/com/stephensalano/fileflow_api/FileflowApiApplication.java
+++ src/main/java/com/stephensalano/fileflow_api/FileflowApiApplication.java
@@ -1,13 +1,23 @@
 package com.stephensalano.fileflow_api;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 public class FileflowApiApplication {
 
+	private static final Logger logger = LoggerFactory.getLogger(FileflowApiApplication.class);
+
 	public static void main(String[] args) {
-		SpringApplication.run(FileflowApiApplication.class, args);
+		SpringApplication app = new SpringApplication(FileflowApiApplication.class);
+		Environment environment = app.run(args).getEnvironment();
+
+		String activeProfiles = String.join(", ", environment.getActiveProfiles());
+		logger.info("Application is running ith profiles: {}", activeProfiles);
 	}
 
 }
